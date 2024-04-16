88fbebf1514fad0766576d766412de71c4cb3640
Thalia Silva <tsilvacorp@gmail.com>
2024-04-15 22:31:05 -0300

    Iniciando o ignite feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
-import './style.css';
+
+import './global.css';
+import styles from './App.module.css';
+
 function App() {
 
   return (
     <div>
       <Header />
-      <Post author="Thalia Silva" content="Esse é um teste"/>
-      <Post author="Fulana de Tal" content="Esse é um teste numero 2"/>
-      <Post author="Beltrana de Y" content="Esse é um teste numero 3"/>
-    
+
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>      
+          <Post author="Thalia Silva" content="Esse é um teste"/>
+          <Post author="Fulana de Tal" content="Esse é um teste numero 2"/>
+          <Post author="Beltrana de Y" content="Esse é um teste numero 3"/>
+    </main>
+      </div>
     </div>
 
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import styles from './Header.module.css';
+import igniteLogo from '../assets/ignite-logo.svg'
+
+console.log(igniteLogo);
 
 export function Header (){
     return (
-        <strong className='header'>Ignite Feed</strong>
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="Logotipo do Ignite" />
+        </header>
     );
 }
