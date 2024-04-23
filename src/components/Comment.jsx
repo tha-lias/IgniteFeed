defd8ddf2b9b12bb32890dfd79121fbe77beeaf5
Thalia da Silva <thalia.silva@rede.local>
2024-04-23 17:57:33 -0300

    Feat: Comment Component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/tha-lias.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                       <div className={styles.authorAndTime}>
+                            <strong>Thalia Silva</strong>
+
+                            <time title="11 de Maio às 08h13" dateTime="2022-05-11 08:13:30"> Cerca de 1h atrás </time>
+                       </div>
+
+                       <button title='Deletar comentário'> <Trash size={20}/> </button>
+                    </header>
+
+                    <p>Muito bom Devon, Parabéns!! 👍</p>
+                </div>
+
+                <footer>
+                    <button> 
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                     </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
