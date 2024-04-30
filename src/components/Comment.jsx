780ced0e4e8a97999ce81590168e021e333aff66
Thalia da Silva <thalia.silva@rede.local>
2024-04-30 17:46:30 -0300

    Feat: : iniciando o uso de useState para controlar os comentários dos posts :construction:

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment() {
+export function Comment({ content }) {
     return (
         <div className={styles.comment}>
 
@@ -20,7 +20,7 @@ export function Comment() {
                        <button title='Deletar comentário'> <Trash size={24}/> </button>
                     </header>
 
-                    <p>Muito bom Devon, Parabéns!! 👍</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,9 +4,17 @@ import {ptBR} from 'date-fns/locale/pt-BR'
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
+import { useState } from 'react';
 
 
 export function Post({author, publishedAt, content}) {
+  const [comments, setComments] = useState(
+    [
+      'Post muito bacana, hein?!'
+    ]
+  )
+  
+  const [newCommentText, setNewCommentText] = useState('')
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -16,6 +24,17 @@ export function Post({author, publishedAt, content}) {
     locale: ptBR,
     addSuffix: true,
   })
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
 
   return (
     <article className={styles.post}>
@@ -47,10 +66,15 @@ export function Post({author, publishedAt, content}) {
    
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback:</strong>
 
-        <textarea placeholder="Deixe o seu comentário"></textarea>
+        <textarea 
+        placeholder="Deixe o seu comentário" 
+        name='inputComentario'
+        value={newCommentText}
+        onChange={handNewCommentChange}
+        ></textarea>
 
         <footer>
         <button type="submit">Publicar</button>
@@ -59,9 +83,11 @@ export function Post({author, publishedAt, content}) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return (
+            <Comment content={comment}/>
+          )
+        })}
       </div>
 
     </article>
